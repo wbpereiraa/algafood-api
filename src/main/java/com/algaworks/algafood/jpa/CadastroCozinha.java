be764e7e920b9370f7b9294a43bc5bd82465356f
William Barbosa <wb.pereiraa@gmail.com>
2024-07-30 21:59:28 -0300

    Criando o método cadastro-cozinha

---
+++ src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
@@ -0,0 +1,32 @@
+package com.algaworks.algafood.jpa;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+@Component
+public class CadastroCozinha {
+	
+	@PersistenceContext
+	private EntityManager manager;
+	
+	public List<Cozinha> listar() {
+		return manager.createQuery("from Cozinha", Cozinha.class).getResultList();
+	}
+	
+	public Cozinha buscar(Long id) {
+		return manager.find(Cozinha.class, id);
+	}
+	
+	@Transactional
+	public Cozinha salvar(Cozinha cozinha) {
+		return manager.merge(cozinha);
+	}
+
+}
