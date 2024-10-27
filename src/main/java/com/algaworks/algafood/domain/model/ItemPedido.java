bd05d5aa055c35c9df2253015266c269eeee934b
William Barbosa <wb.pereiraa@gmail.com>
2024-10-27 12:42:25 -0300

    Criando as entidades ItemPedido, Pedido e o Enum StatusPedido

---
+++ src/main/java/com/algaworks/algafood/domain/model/ItemPedido.java
@@ -0,0 +1,36 @@
+package com.algaworks.algafood.domain.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class ItemPedido {
+
+	 @EqualsAndHashCode.Include
+	    @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id;
+
+	    private BigDecimal precoUnitario;
+	    private BigDecimal precoTotal;
+	    private Integer quantidade;
+	    private String observacao;
+
+	    @ManyToOne
+	    @JoinColumn(nullable = false)
+	    private Pedido pedido;
+
+	    @ManyToOne
+	    @JoinColumn(nullable = false)
+	    private Produto produto;
+}
